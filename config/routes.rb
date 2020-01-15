2d5fee97257981dac96d7d6162cdb8b957dba310
phalado <phalado@gmail.com>
2020-01-15 19:37:47 -0300

    Created some pages and some style for users and navbar

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/show'
   post 'posts/new', to: 'posts#create'
-  resources :posts, only: [:new, :create, :index, :show]
-  
+  resources  :posts, only: [:new, :create, :index, :show]
+  resources  :user, only: [:show]
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'user#index'
