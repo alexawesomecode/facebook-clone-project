7fed4611860683215993fffdbc36c15f7eb403b9
phalado <phalado@gmail.com>
2020-01-16 14:19:31 -0300

    Added some tests for users

--- db/migrate/20200115193935_add_first_name_to_user.rb
+++ db/migrate/20200115193935_add_first_name_to_user.rb
@@ -2,8 +2,6 @@ class AddFirstNameToUser < ActiveRecord::Migration[6.0]
   def change
     add_column :users, :first_name, :string
     add_column :users, :last_name, :string
-    add_column :users, :password, :string
-    add_column :users, :password_confirmation, :string
     add_column :users, :birthday, :date
     add_column :users, :image, :string
     
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "returns false if try to save user without partameters" do
+    user = User.new
+    assert !user.valid?
+  end
+
+  it "returns true if try to save user with partameters" do
+    user = User.new(email: "user@example.com", password: "foobar")
+    assert user.valid?
+  end
+
+  it "returns false if try to save two users with the same partameters" do
+    user1 = User.new(email: "user@example.com", password: "foobar")
+    user1.save
+    user2 = user1.dup
+    assert !user2.valid?
+  end
 end
