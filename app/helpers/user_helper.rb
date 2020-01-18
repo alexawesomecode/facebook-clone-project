3592955220fe61b114da45a81bf042b32c197261
phalado <phalado@gmail.com>
2020-01-18 16:43:05 -0300

    Done some styling

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,7 +1,7 @@
 module UserHelper
 
   # Returns the Gravatar for the given user.
-  def gravatar_for(user, options = { size: 80 })
+  def gravatar_for(user, options = { size: 200 })
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
