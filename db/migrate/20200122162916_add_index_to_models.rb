0ce98e8eaccd0b46504f631330f1c89d09200b82
phalado <phalado@gmail.com>
2020-01-22 13:50:28 -0300

    Added more indexes in the tables

---
+++ db/migrate/20200122162916_add_index_to_models.rb
@@ -0,0 +1,18 @@
+class AddIndexToModels < ActiveRecord::Migration[6.0]
+  def change
+    # Friendships table
+    add_index :friendships, :sender
+    add_index :friendships, :receiver
+
+    # Comment_likes table
+    add_index :comment_likes, :user_id
+    add_index :comment_likes, :comment_id
+
+    # Comments table
+    add_index :comments, :post_id
+
+    # Postlikes table
+    add_index :postlikes, :user_id
+    add_index :postlikes, :post_id
+  end
+end
