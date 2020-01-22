0c3567e5f3850b86b51d2a3d3ddf127604383510
alejandro-andres <alejandro-andres@debian.alex>
2020-01-22 17:23:43 -0300

    added omniauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+         :omniauthable, omniauth_providers: %i[facebook]
 
   # Associationd with Post and Postlike
   has_many :posts, foreign_key: 'creator'
@@ -20,4 +21,25 @@ class User < ApplicationRecord
   
   has_many :senders, class_name: 'Friendship', foreign_key: 'sender'
   has_many :receivers, class_name: 'Friendship', foreign_key: 'receiver'
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name   # assuming the user model has a name
+      user.image = auth.info.image # assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails, 
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+  
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,4 +296,7 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
+   
+    #omniouth for facebook
+  config.omniauth :facebook, "1286724961522874", "2a6eb2af41b535dacd6f670aeaaf816f"
 end
