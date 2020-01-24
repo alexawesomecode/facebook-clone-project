bbf914d467011d32639f2efc7680275f17a59bca
phalado <phalado@gmail.com>
2020-01-24 13:52:02 -0300

    Went back some commits, now everything is working

--- config/routes.rb
+++ config/routes.rb
@@ -5,19 +5,19 @@ Rails.application.routes.draw do
   get  'posts/index'
   get  'posts/show'
   root 'posts#index'
-  get  '/help',                    to:   'static_pages#help'
-  get  '/about',                   to:   'static_pages#about'
-  get  '/contact',                 to:   'static_pages#contact'
-  post 'posts/new',                to:   'posts#create'
-  post 'comments/new',             to:   'comments#create'
-  post 'friendships/new',          to:   'friendships#create'
-  get 'friendships/notifications', to:   'friendships#notifications'
-  resources  :posts,               only: [:new, :create, :index, :show]
-  resources  :user,                only: [:show, :index]
-  resources  :comments,            only: [:new, :create, :destroy]
-  resources  :postlikes,           only: [:new, :create, :destroy]
-  resources  :friendships,         only: [:new, :create, :destroy, :edit, :index, :show]
-  resources  :commentlike,         only: [:new, :create, :destroy]
+  get  '/help',                     to:   'static_pages#help'
+  get  '/about',                    to:   'static_pages#about'
+  get  '/contact',                  to:   'static_pages#contact'
+  post 'posts/new',                 to:   'posts#create'
+  post 'comments/new',              to:   'comments#create'
+  post 'friendships/new',           to:   'friendships#create'
+  get  'friendships/notifications', to:   'friendships#notifications'
+  resources  :posts,                only: [:new, :create, :index, :show]
+  resources  :user,                 only: [:show, :index]
+  resources  :comments,             only: [:new, :create, :destroy]
+  resources  :postlikes,            only: [:new, :create, :destroy]
+  resources  :friendships,          only: [:new, :create, :destroy, :edit, :index, :show]
+  resources  :commentlike,          only: [:new, :create, :destroy]
   resources  :posts do
     resources  :comments,          only: [:new, :create, :destroy]
   end
