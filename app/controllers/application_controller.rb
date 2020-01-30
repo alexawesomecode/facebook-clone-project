024d424c4360fb910392b99f49005e57a20118f0
phalado <phalado@gmail.com>
2020-01-29 21:25:41 -0300

    Some 'fix' in the password problem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,7 +28,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image birthday])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image birthday])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image birthday password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image birthday password])
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -26,7 +26,8 @@ class User < ApplicationRecord
                          provider: auth.provider,
                          uid: auth.uid,
                          email: auth.info.email,
-                         password: Devise.friendly_token[0, 20])
+                         password: '123456',
+                         birthday: Date.today)
     user
   end
 
